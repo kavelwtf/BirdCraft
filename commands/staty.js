a0a3cd73547e2ff91769c7d76c68d03e10d9bf39
Kamil Przybysławski <89796140+kavelwtf@users.noreply.github.com>
2021-09-20 20:59:36 +0200

    Update staty.js

--- commands/staty.js
+++ commands/staty.js
@@ -1,1 +1,15 @@
 
+module.exports = client => {
+    const channelId = '889505818863628359'
+
+    const updateMembers = guild => {
+        const channel = guild.channels.cache.get(channelId)
+        channel.setName(`Użytkownicy: ${guild.memberCount.toLocaleString()}`)
+    }
+
+    client.on('guildMemberAdd', (member) => updateMembers(member.guild))
+    client.on('guildMemberRemove', (member) => updateMembers(member.guild))
+
+    const guild = client.guilds.cache.get('888705348859494400')
+    updateMembers(guild)
+}
