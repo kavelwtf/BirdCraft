ac828010adee4430b92e57e8fe76406ac7b107c2
Kamil Przybysławski <89796140+kavelwtf@users.noreply.github.com>
2021-09-20 21:45:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ client.on("ready", () => {
     console.log("Bot zostal poprawnie uruchomiony");
     //Ustawianie statusu
     client.user.setActivity("https://www.BirdCraft.pl/", {
-        type: "WATCHING"
+        type: "WATCHING",
+        status: 'idle'
       });
     });
 
@@ -31,22 +32,6 @@ client.on("guildMemberAdd", member =>{
     
     channel.send(embed);
     });
-
-module.exports = client => {
-    commands: 'staty'
-    const channelId = '889505818863628359'
-
-    const updateMembers = guild => {
-        const channel = guild.channels.cache.get(channelId)
-        channel.setName(`Użytkownicy: ${guild.memberCount.toLocaleString()}`)
-    }
-
-    client.on('guildMemberAdd', (member) => updateMembers(member.guild))
-    client.on('guildMemberRemove', (member) => updateMembers(member.guild))
-
-    const guild = client.guilds.cache.get('888705348859494400')
-    updateMembers(guild)
-}
 
 client.on("message",  message => {
    if(message.channel.id === '888808271715123240') {
