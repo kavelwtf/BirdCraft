43957d0f8aa9a4c65d990231e1295c449478a45a
Kamil Przybysławski <89796140+kavelwtf@users.noreply.github.com>
2021-09-20 21:08:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,22 @@ client.on("guildMemberAdd", member =>{
     channel.send(embed);
     });
 
+module.exports = client => {
+    commands: 'staty'
+    const channelId = '889505818863628359'
+
+    const updateMembers = guild => {
+        const channel = guild.channels.cache.get(channelId)
+        channel.setName(`Użytkownicy: ${guild.memberCount.toLocaleString()}`)
+    }
+
+    client.on('guildMemberAdd', (member) => updateMembers(member.guild))
+    client.on('guildMemberRemove', (member) => updateMembers(member.guild))
+
+    const guild = client.guilds.cache.get('888705348859494400')
+    updateMembers(guild)
+}
+
 client.on("message",  message => {
    if(message.channel.id === '888808271715123240') {
         //message.react(`✅`);
